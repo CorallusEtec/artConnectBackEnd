88601badbeffb02e9a9e282229bd61357112ccaf
SamuMeneDev <samuelsantiago2222@gmail.com>
2026-02-20 16:54:12 -0300

    Implementação inicial do service e método find

---
+++ src/main/java/corallus/artConnect/artConnect/service/ArteService.java
@@ -0,0 +1,33 @@
+package corallus.artConnect.artConnect.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import corallus.artConnect.artConnect.entity.Artista;
+import corallus.artConnect.artConnect.repository.ArtistasRepository;
+
+@Service
+public class ArteService {
+	
+	@Autowired
+	private ArtistasRepository artistaRepository;
+	
+	
+	public Artista find(String email, String senha) {
+		List<Artista> lista = artistaRepository.findAll();
+		Artista a = new Artista(email, senha);
+		a = this.encontrarArtista(a, lista);
+		return a;
+	}
+	
+	private Artista encontrarArtista(Artista consulta, List<Artista> lista) {
+		for(int i=0; i<lista.size(); i++) {
+			if(lista.get(i).equals(consulta)) {
+				return lista.get(i);
+			}
+		}
+		return null;
+	}
+}
