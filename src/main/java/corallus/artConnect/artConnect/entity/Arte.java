79ced36818fa49d87cdcc48c82742dd25823340f
Samu Mene <samuelsantiago2222@gmail.com>
2026-02-20 11:31:52 -0300

    Criar Entidades 1/2

---
+++ src/main/java/corallus/artConnect/artConnect/entity/Arte.java
@@ -0,0 +1,5 @@
+package corallus.artConnect.artConnect.entity;
+
+public class Arte {
+
+}
---
+++ src/main/java/corallus/artConnect/artConnect/entity/Usuario.java
@@ -0,0 +1,19 @@
+package corallus.artConnect.artConnect.entity;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class Usuario {
+	private String nome;
+	private String email;
+	private String senha;
+	private List<String> telefone;
+	private Endereco endereco;
+}
