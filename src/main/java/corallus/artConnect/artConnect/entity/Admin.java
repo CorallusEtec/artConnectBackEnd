bca7dea088d673d1e0bf17b7955919ab08c0762c
Samu Mene <samuelsantiago2222@gmail.com>
2026-02-21 18:25:55 -0300

    terminando adm q faltava

--- src/main/java/corallus/artConnect/artConnect/entity/Admin.java
+++ src/main/java/corallus/artConnect/artConnect/entity/Admin.java
@@ -1,5 +1,12 @@
 package corallus.artConnect.artConnect.entity;
 
-public class Admin extends Usuario{
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor @AllArgsConstructor
+@Getter @Setter
+public class Admin extends Usuario{
+	private Long id;
 }
