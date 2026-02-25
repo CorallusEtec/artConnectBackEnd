0db8f445ec35e0b6649e6c5cb0702a7e7d8e67e8
Samu Mene <samuelsantiago2222@gmail.com>
2026-02-25 12:24:37 -0300

    Implementação de login finalizada

--- src/main/java/corallus/artConnect/artConnect/controller/LoginController.java
+++ src/main/java/corallus/artConnect/artConnect/controller/LoginController.java
@@ -3,7 +3,9 @@ package corallus.artConnect.artConnect.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import corallus.artConnect.artConnect.entity.Usuario;
@@ -14,11 +16,14 @@ import corallus.artConnect.artConnect.service.LoginService;
 public class LoginController {
     @Autowired
     private LoginService loginService;
-    public ResponseEntity<Usuario> login(String email, String senha) {
+
+    @GetMapping("/logar")
+    public ResponseEntity<Usuario> login(@RequestParam String email, @RequestParam String senha) {
         try {
             Usuario usuario = loginService.login(email, senha);
             return new ResponseEntity<>(usuario, HttpStatus.FOUND);
         } catch (Exception e) {
+            e.printStackTrace();
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
