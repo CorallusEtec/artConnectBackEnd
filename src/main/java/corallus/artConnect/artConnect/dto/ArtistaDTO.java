464700751180f571acbce9358291b92450fcb57a
SamuMeneDev <samuelsantiago2222@gmail.com>
2026-03-27 14:40:02 -0300

    primeiras DTOS do artista

---
+++ src/main/java/corallus/artConnect/artConnect/dto/ArtistaDTO.java
@@ -0,0 +1,31 @@
+package corallus.artConnect.artConnect.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import corallus.artConnect.artConnect.entity.Arte;
+import corallus.artConnect.artConnect.entity.contato.ContatoArtista;
+import corallus.artConnect.artConnect.entity.publicacao.Publicacao;
+
+public record ArtistaDTO(
+		Long id,
+		String nome,
+		String email,
+		String nomeArtistico,
+		LocalDate dataNasc,
+		Character sexo,
+		
+		// LOGRADOURO / ENDEREÇO
+		
+		String nomeLog,
+		Short numLog,
+		String cep,
+		String bairro,
+		String complemento,
+		String cidade,
+		String uf,
+		
+		Arte arte,
+		List<ContatoArtista> contatos,
+		List<Publicacao> publicacoes
+) {}
