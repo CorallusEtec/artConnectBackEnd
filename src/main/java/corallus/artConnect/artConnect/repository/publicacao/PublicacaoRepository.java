594b89f6800c48fbe66a68846fc54642a4184006
rfael-SdM <rafaelsoutodemoura09@gmail.com>
2026-04-30 16:51:55 -0300

    correção ordem listagem das publicações

--- src/main/java/corallus/artConnect/artConnect/repository/publicacao/PublicacaoRepository.java
+++ src/main/java/corallus/artConnect/artConnect/repository/publicacao/PublicacaoRepository.java
@@ -1,9 +1,11 @@
 package corallus.artConnect.artConnect.repository.publicacao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import corallus.artConnect.artConnect.entity.publicacao.Publicacao;
 
 public interface PublicacaoRepository extends JpaRepository<Publicacao, Long>{
-    
+	List<Publicacao> findByStatusPublicacao_TipoStatus_IdOrderByDataPublicacaoDesc(Long id);
 }
