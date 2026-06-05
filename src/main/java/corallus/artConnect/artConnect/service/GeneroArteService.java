9d33c4a4d1717fce8d98d3131c5e4cda09111cf0
Rafael <rafaelsoutodemoura09@gmail.com>
2026-06-05 20:27:34 -0300

    entidade e crud basico genero de arte

---
+++ src/main/java/corallus/artConnect/artConnect/service/GeneroArteService.java
@@ -0,0 +1,40 @@
+package corallus.artConnect.artConnect.service;
+
+import corallus.artConnect.artConnect.dto.request.arte.GeneroArteSaveRequest;
+import corallus.artConnect.artConnect.dto.response.util.MessageResponse;
+import corallus.artConnect.artConnect.entity.arte.Arte;
+import corallus.artConnect.artConnect.entity.arte.GeneroArte;
+import corallus.artConnect.artConnect.error.errors.ArteNotFoundException;
+import corallus.artConnect.artConnect.repository.arte.ArteRepository;
+import corallus.artConnect.artConnect.repository.arte.GeneroArteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GeneroArteService {
+
+    private final GeneroArteRepository generoArteRepository;
+    private final ArteRepository arteRepository;
+
+    public GeneroArteService(GeneroArteRepository generoArteRepository, ArteRepository arteRepository) {
+        this.generoArteRepository = generoArteRepository;
+        this.arteRepository = arteRepository;
+    }
+
+    public List<GeneroArte> findAll() {
+        return this.generoArteRepository.findAll();
+    }
+
+    public MessageResponse save(GeneroArteSaveRequest saveRequest) {
+        Arte arte = this.arteRepository.findById(saveRequest.arteId())
+        .orElseThrow(ArteNotFoundException::new);
+
+        GeneroArte generoArte = new GeneroArte();
+        generoArte.setNomeGeneroArte(saveRequest.nomeGeneroArte());
+        generoArte.setArte(arte);
+
+        this.generoArteRepository.save(generoArte);
+        return new MessageResponse("Gênero de arte criado.");
+    }
+}
