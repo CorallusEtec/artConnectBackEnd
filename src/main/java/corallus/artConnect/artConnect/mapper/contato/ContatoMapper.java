4dc3836688f5de6e23015f5e4756ab9f5ebfe4b5
Rafael <rafaelsoutodemoura09@gmail.com>
2026-06-19 17:12:24 -0300

    corrigindo response dos contatos em get

--- src/main/java/corallus/artConnect/artConnect/mapper/contato/ContatoMapper.java
+++ src/main/java/corallus/artConnect/artConnect/mapper/contato/ContatoMapper.java
@@ -2,9 +2,18 @@ package corallus.artConnect.artConnect.mapper.contato;
 
 import corallus.artConnect.artConnect.dto.response.contato.ContatoResponse;
 import corallus.artConnect.artConnect.entity.contato.Contato;
-import corallus.artConnect.artConnect.mapper.BaseMapper;
+import corallus.artConnect.artConnect.mapper.tipoContato.TipoContatoMapper;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import java.util.List;
 
-@Mapper(componentModel = "spring")
-public interface ContatoMapper extends BaseMapper<Contato, ContatoResponse> {
+@Mapper(componentModel = "spring", uses = { TipoContatoMapper.class })
+public interface ContatoMapper {
+    
+    @Mapping(source = "id", target = "idContato")
+    @Mapping(source = "tipoContato", target = "tipoContato")
+    @Mapping(source = "valorContato", target = "valorContato")
+    ContatoResponse toDTO(Contato entity);
+    
+    List<ContatoResponse> toDTOList(List<Contato> entityList);
 }
