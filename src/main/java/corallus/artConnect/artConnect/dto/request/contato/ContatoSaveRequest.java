684adafe61e93881b5e6513fc829354a53e9146d
NkAndre <andre270708@gmail.com>
2026-06-18 21:57:29 -0300

    fix: corrige validacao do idTipoContato para NotNull

--- src/main/java/corallus/artConnect/artConnect/dto/request/contato/ContatoSaveRequest.java
+++ src/main/java/corallus/artConnect/artConnect/dto/request/contato/ContatoSaveRequest.java
@@ -1,9 +1,10 @@
 package corallus.artConnect.artConnect.dto.request.contato;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record ContatoSaveRequest(
-        @NotBlank(message = "Não foi possível salvar o contato")
+        @NotNull(message = "Não foi possível salvar o contato")
         Long idTipoContato,
 
         String valorContato
