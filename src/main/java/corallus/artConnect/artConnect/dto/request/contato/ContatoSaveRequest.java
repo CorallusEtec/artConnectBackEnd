fb1b307813de5763e56896da524bdaed0523abf9
NkAndre <andre270708@gmail.com>
2026-06-18 22:34:13 -0300

    tst

--- src/main/java/corallus/artConnect/artConnect/dto/request/contato/ContatoSaveRequest.java
+++ src/main/java/corallus/artConnect/artConnect/dto/request/contato/ContatoSaveRequest.java
@@ -3,6 +3,7 @@ package corallus.artConnect.artConnect.dto.request.contato;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
+
 public record ContatoSaveRequest(
         @NotNull(message = "Não foi possível salvar o contato")
         Long idTipoContato,
