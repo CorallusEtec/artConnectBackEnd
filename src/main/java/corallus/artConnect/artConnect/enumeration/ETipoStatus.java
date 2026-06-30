bfebdd2d4439fe462f829f01a2bdaa5fae5db45c
SamuMeneDev <samuelsantiago2222@gmail.com>
2026-06-29 23:19:46 -0300

    Endpoints de criação e listagem de denuncias

--- src/main/java/corallus/artConnect/artConnect/enumeration/ETipoStatus.java
+++ src/main/java/corallus/artConnect/artConnect/enumeration/ETipoStatus.java
@@ -4,5 +4,13 @@ public enum ETipoStatus {
     ATIVO,
     PENDENTE,
     SUSPENSO,
-    EXCLUIDO
+    EXCLUIDO,
+    BANIDO,
+    ARQUIVADO,
+    CONCLUIDO;
+
+    public static final String tipoStatusDenunciaRegEx = "ARQUIVADO|CONCLUIDO|ATIVO";
+    public static final String tipoStatusUsuarioRegEx = "SUSPENSO|BANIDO|ATIVO";
+    public static final String tipoStatusComentarioRegEx = "EXCLUIDO|ATIVO";
+    public static final String tipoStatusPublicacaoRegEx = "EXCLUIDO|ATIVO";
 }
---
+++ src/main/java/corallus/artConnect/artConnect/repository/DenunciaRepository.java
@@ -0,0 +1,7 @@
+package corallus.artConnect.artConnect.repository;
+
+import corallus.artConnect.artConnect.entity.Denuncia;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DenunciaRepository extends JpaRepository<Denuncia, Long> {
+}
